577750474286b3116329f0f1053e72145b123830
jtdejesus <157234255+jtdejesus@users.noreply.github.com>
2024-05-26 16:04:43 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    var Line1 = ("Coming out of my cage");
+    var Line2 = ("And I've been doing just fine");
+    var Line3 = ("Gotta gotta be down");
+    var Line4 = ("Because I want it all");
+    Console.WriteLine (Line1);
+    Console.WriteLine (Line2);
+    Console.WriteLine (Line3);
+    Console.WriteLine (Line4);
+  }
+}
